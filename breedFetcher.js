5d729b86f3f9eb2e16e2596dfc7cefa655785661
osmanmm <mohamed.osman@outlook.com>
2021-09-06 17:43:13 +0000

    add breedFetcherTest file using mocah and chai for test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,21 @@
 const request = require("request");
-request("https://aa.zzz", (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  console.log("error:", error); // Print the error if one occurred
-  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("not found");
-    return;
-  }
-  console.log(data.length);
-  console.log(data[0].description);
-  console.log(typeof data);
-});
+
+const fetchBreedDescription = (breedName, callback) => {
+  request(
+    "https://api.thecatapi.com/v1/breeds/search?q=" + breedName,
+    (error, response, body) => {
+      if (error) {
+        return callback(error);
+      } else {
+        const data = JSON.parse(body);
+        if (data.length === 0) {
+          return callback("Not valid");
+        } else {
+          return callback(null, data[0].description);
+        }
+      }
+    }
+  );
+};
+
+module.exports = { fetchBreedDescription };
